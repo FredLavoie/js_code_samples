606dd767eb7a9c81e1f2ac1e6dc7e44528b8ebe3
FredLavoie <flavoie3@gmail.com>
2019-08-24 06:40:39 -0400

    Add eslint

--- spread_operator.js
+++ spread_operator.js
@@ -2,7 +2,7 @@ function minMax(arr){
   return [Math.min(...arr), Math.max(...arr)];
 }
 
-
+console.log('find min and max in an array');
 console.log(minMax([1,2,3,4,5]));
 console.log(minMax([134533,5]));
 console.log(minMax([1]));
