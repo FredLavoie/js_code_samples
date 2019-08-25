d5c1433a57aa93f9f75c3f8f77ba8336f223360a
FredLavoie <flavoie3@gmail.com>
2019-08-25 12:33:57 -0400

    Completed filter method examples

--- samples/filter_function.js
+++ samples/filter_function.js
@@ -1,7 +1,7 @@
 // filter out the even numbers
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let filtered1 = numbers.filter(number => {
+let filtered1 = arr1.filter(number => {
   return number % 2 === 0;
 });
 
@@ -9,12 +9,31 @@ console.log('filter out the even numbers');
 console.log(filtered1);
 
 /****************************************************************************/
-// dfg
+// filter out the numbers smaller than 35
 
-const arr = [,,,,,,,];
+const arr3 = [12, 5, 8, 130, 44];
 
-let filtered2 = arr.filter(ea => {
+function isBigEnough(value) {
+  return value >= 10;
+}
 
-});
+var filtered3 = arr3.filter(isBigEnough);
 
-console.log(filtered2);
+console.log('filter out numbers greater than 10');
+console.log(filtered3);
+
+
+/****************************************************************************/
+// filter built into function with criteria input
+
+const arr4 = ['banana', 'apple', 'blueberry', 'orange', 'cherry'];
+
+function filterByCriteria(array, test) {
+  return array.filter(ea => ea.includes(test));
+}
+console.log('function with filter built in to search by creteria');
+console.log('return items containing "rry" in the name:');
+console.log(filterByCriteria(arr4, 'rry'));
+console.log('return items containing the letter "a" in the name:');
+console.log(filterByCriteria(arr4, 'a'));
+
