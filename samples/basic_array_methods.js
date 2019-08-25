421c2b6ea6495b4eae09eea0c454a5738bc3fb07
FredLavoie <flavoie3@gmail.com>
2019-08-25 12:08:54 -0400

    Finish basic array method example

--- samples/basic_array_methods.js
+++ samples/basic_array_methods.js
@@ -1,30 +1,24 @@
 
-// These array methods are NOT pure, the modify the array
+// These array methods are NOT pure, they modify the array
 
 /****************************************************************************/
 // array 'shift' and 'unshift'
 // Adds and removes items from the START of the array
 
-const arr1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
-console.log(arr1);
-arr1.shift('apple', 'banana', 'blueberry');
-console.log(arr1);
-arr1.unshift();
-
+const arr1 = ['a', 'b', 'c', 'd'];
+console.log('original array   =>', arr1);
+arr1.unshift('apple');
+console.log('after unshift()  =>',arr1);
+arr1.shift();
+console.log('after shift()    =>', arr1);
 
 /****************************************************************************/
 // array 'push' and 'pop'
 // Adds and removes items from the END of the array
 
-const arr2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
-console.log(arr2);
-arr2.push('apple', 'banana', 'blueberry');
-console.log(arr2);
+const arr2 = ['a', 'b', 'c', 'd'];
+console.log('original array   =>', arr2);
+arr2.push('apple');
+console.log('after push()     =>',arr2);
 arr2.pop();
-console.log(arr2);
-
-/****************************************************************************/
-// array 'indexOf' and 'lastIndexOf'
-// finds the first index and the last index of an item in an array
-
-
+console.log('after pop()      =>', arr2);
