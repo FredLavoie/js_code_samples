2426c13ab6e7288942e8742cb784313c36c00916
FredLavoie <flavoie3@gmail.com>
2019-10-15 17:56:04 -0400

    Add fs module test that demonstrates sync vs async

---
+++ fs_module_test/fs_script.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs');
+
+
+// implementation of readFileSync
+console.log('readFileSync started');
+const data = fs.readFileSync('text_file.txt');
+console.log(data.toString());
+console.log('readFileSync ended');
+
+
+// implementation of readFile 
+console.log('readFile started');
+fs.readFile('text_file.txt', function (err, data) {
+    if (err) {
+		return console.error(err);
+	}
+	console.log(data.toString());
+	console.log('readFile ended');
+
+});
+console.log('code after readFile');
+
+/********************************************************/
+// readFileSync is blocking, and therefore doesn't allow
+// the rest of the code to run until it's done with its
+// operation.
+
+// readFile is non-blocking and allows the rest of the 
+// code to continue running and only once done reading
+// does it execute the callback function.
